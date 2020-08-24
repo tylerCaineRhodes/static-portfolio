26cc1ceafc23b8e527cfb43b3b68823202447158
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-08-24 15:15:39 -0500

    Fix image zoom

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const parallaxElems = document.querySelectorAll('.parallax');
   const sideNavElems = document.querySelectorAll('.sidenav');
   const scrollspyElems = document.querySelectorAll('.scrollspy')
+  const imageBoxElems = document.querySelectorAll('.materialboxed');
 
   M.Parallax.init(parallaxElems);
   M.Sidenav.init(sideNavElems);
   M.ScrollSpy.init(scrollspyElems);
+  M.Materialbox.init(imageBoxElems);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -14,4 +16,4 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 const musicResumeButton = document.querySelector('#musicResume');
-musicResumeButton.addEventListener('click', () => alert('Sorry, my latest resume is not updated!'));
+musicResumeButton.addEventListener('click', () => alert('Sorry, my latest resume isn\'t updated!'));
