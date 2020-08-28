8bd3328a440d292a162339dc70203b7f85d98e20
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-08-27 21:03:20 -0500

    Add media queries to topbar logo

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -15,5 +15,36 @@ document.addEventListener('DOMContentLoaded', function () {
   M.Modal.init(modalElems);
 });
 
-const musicResumeButton = document.querySelector('#musicResume');
-musicResumeButton.addEventListener('click', () => alert('Sorry, my latest resume isn\'t updated!'));
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('#slyimage-chan');
+
+const checkSlide = (e) => {
+  sliderImages.forEach((image) => {
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    const isHalfShown = slideInAt > image.offsetTop;
+
+    if (isHalfShown) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
